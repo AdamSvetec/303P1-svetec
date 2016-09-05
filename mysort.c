1a8b456ddef0f07c3f6f731773119421e472c2a7
AdamSvetec <adamsvetec@gmail.com>
2016-09-04 21:08:17 -0400

    my sort implementation

--- mysort.c
+++ mysort.c
@@ -6,14 +6,82 @@
 #include <string.h>
 #include "support.h"
 
+/* Function used to compare lines */ 
+int compare_function(const void * a, const void * b){
+  return strcmp(*(char**)a, *(char**)b);
+}
+
+void print_lines(char ** lines, int length){
+  for(int i = 0; i < length; i++){
+    fprintf(stderr,"%s",lines[i]);
+  }
+}
+
+/* Removes identical strings from array */
+void make_unique(char ** lines, int *length){
+  for(int pointer = 1; pointer < *length; pointer++){
+    if(strcmp(lines[pointer], lines[pointer-1]) == 0){
+      free(lines[pointer-1]);
+      for(int shifter = pointer; shifter < *length; shifter++){
+	lines[shifter-1] = lines[shifter];
+      }
+      (*length)--;
+      pointer--;
+    }
+  }
+}
+
+/* Reverses lines */
+void reverse_arr(char ** lines, int length){
+  char * temp;
+  int start = 0;
+  int end = length - 1;
+  while(start < end){
+    temp = lines[start];
+    lines[start] = lines[end];
+    lines[end] = temp;
+    start++;
+    end--;
+  }
+}
+
 /*
  * sort_file() - read a file, sort its lines, and output them.  reverse and
  *               unique parameters are booleans to affect how the lines are
  *               sorted, and which lines are output.
  */
 void sort_file(char *filename, int unique, int reverse) {
-    /* TODO: Complete this function */
-    /* Note: you will probably need to implement some other functions */
+  char ** lines;
+  lines = malloc(1000000 * sizeof(char*));
+
+  FILE * fp;
+  fp = fopen(filename, "r");
+  if(fp == NULL)
+    exit(1);
+
+  int line_num = 0;
+  lines[line_num] = (char *)malloc(1024*sizeof(char));
+  while(fgets(lines[line_num], 1024, fp) != NULL){
+    line_num++;
+    lines[line_num] = (char *)malloc(1024*sizeof(char));
+  }
+  fclose(fp);
+  
+  qsort(lines, line_num, sizeof(char*), compare_function);
+
+  if(unique)
+    make_unique(lines, &line_num);
+
+  if(reverse)
+    reverse_arr(lines,line_num);
+  
+  int pointer = 0;
+  while(pointer < line_num){
+    printf("%s", lines[pointer]);
+    free(lines[pointer]);
+    pointer++;
+  }
+  free(lines);
 }
 
 /*
@@ -33,6 +101,8 @@ void help(char *progname) {
 int main(int argc, char **argv) {
     /* for getopt */
     long opt;
+    int reverse = 0;
+    int unique = 0;
 
     /* ensure the student name is filled out */
     check_student(argv[0]);
@@ -40,13 +110,13 @@ int main(int argc, char **argv) {
     /* parse the command-line options.  They are 'r' for reversing the */
     /* output, and 'u' for only printing unique strings.  'h' is also */
     /* supported. */
-    /* TODO: parse the arguments correctly */
-    while ((opt = getopt(argc, argv, "h")) != -1) {
+    while ((opt = getopt(argc, argv, "hru")) != -1) {
         switch(opt) {
           case 'h': help(argv[0]); break;
-        }
+	  case 'r': reverse = 1; break;
+	  case 'u': unique = 1; break;
+	}
     }
 
-    /* TODO: fix this invocation */
-    sort_file(argv[optind], 0, 0);
+    sort_file(argv[optind], unique, reverse);
 }
