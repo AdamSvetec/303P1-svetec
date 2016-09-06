4fccd152b51087863b830feda784913402b81c8e
AdamSvetec <adamsvetec@gmail.com>
2016-09-06 13:26:30 -0400

    WIP mysed implementation     -needs to be cleaned up

--- mysed.c
+++ mysed.c
@@ -4,12 +4,89 @@
 #include <string.h>
 #include "support.h"
 
+int check_pattern(const char * pattern, char * find, char * replace_with){
+  int char_pointer = 0;
+  if(pattern[char_pointer] == '\0' || pattern[char_pointer] != 's')
+    return 0;
+  char_pointer++;
+  if(pattern[char_pointer] == '\0' || pattern[char_pointer] != '/')
+    return 0;
+  char_pointer++;
+  int start = char_pointer;
+  while(pattern[char_pointer] != '\0' && pattern[char_pointer] != '/'){
+    find[char_pointer-start] = pattern[char_pointer];
+    char_pointer++;
+  }
+  find[char_pointer-start]='\0';
+  if(pattern[char_pointer] == '\0')
+    return 0;
+  char_pointer++;
+  start = char_pointer;
+  while(pattern[char_pointer] != '\0' && pattern[char_pointer] != '/'){
+    replace_with[char_pointer-start] = pattern[char_pointer];
+    char_pointer++;
+  }
+  replace_with[char_pointer-start]='\0';
+  if(pattern[char_pointer] == '\0')
+    return 0;
+  char_pointer++;
+  if(pattern[char_pointer] == '\0' || pattern[char_pointer] != 'g')
+    return 0;
+  char_pointer++;
+  if(pattern[char_pointer] == '\0')
+    return 1;
+  return 0;
+}
+
+char * replace_str(char *str, char *find, char *replace){
+  static char buffer[2048];
+  char *p;
+  if(!(p = strstr(str, find)))
+    return str;
+  strncpy(buffer, str, p-str);
+  buffer[p-str] = '\0';
+  sprintf(buffer+(p-str), "%s%s", replace, p+strlen(find));
+  return buffer;
+}
+
 /*
  * sed_file() - modify lines of filename based on pattern, and print result
  *              to STDOUT
  */
 void sed_file(char *filename, char *pattern) {
-    /* TODO: Complete this function */
+  if(pattern == NULL){
+    printf("Please enter pattern");
+    exit(1);
+  }
+  char find [1024], replace_with [1024];
+  int valid = check_pattern(pattern, find, replace_with);
+  if(!valid){
+    printf("Error: Pattern is not formatted correctly");
+    exit(1);
+  }
+
+  FILE * fp;
+  if(filename == NULL){
+    fp = stdin;
+  }else{
+    fp = fopen(filename, "r");
+  }
+  if(fp == NULL){
+    printf("Error: File could not be opened");
+    exit(1);
+  }
+
+  char line [2048];
+  while(fgets(line, 2048, fp) != NULL){
+    char * newstr;
+    newstr = replace_str(line, find, replace_with);
+    while(0 != strcmp(newstr, line)){
+      strcpy(line, newstr);
+      newstr = replace_str(line, find, replace_with);
+    }
+    printf("%s", newstr);
+  }
+  fclose(fp);
 }
 
 /*
@@ -28,7 +105,6 @@ void help(char *progname) {
 int main(int argc, char **argv) {
     /* for getopt */
     long opt;
-
     /* run a student name check */
     check_student(argv[0]);
 
@@ -39,7 +115,6 @@ int main(int argc, char **argv) {
           case 'h': help(argv[0]); break;
         }
     }
-
-    /* TODO: fix this invocation */
-    sed_file(NULL, NULL);
+    
+    sed_file(argv[2],argv[1]);
 }
